c07c1a431895a3195fb41eacf7980ca37a81dce7
Isaac Garcia <iisaacgg@gmail.com>
2023-06-23 13:48:01 -0600

    Guardar archivos txt y json
    Al intentar de diferentes formas usar archivos zip, deja de funcionar, (parte comentada del codigo en html)

--- Interfaz/test.js
+++ Interfaz/test.js
@@ -1,18 +1,19 @@
-function procesarArchivo() {
-    var archivoInput = document.getElementById('archivoZip');
-    var archivo = archivoInput.files[0];
-  
-    if (archivo) {
-      var lector = new FileReader();
-      lector.onload = function(e) {
-        var contenido = e.target.result;
-  
-        // Guardar el contenido en el textarea
-        var textarea = document.getElementById('exampleFormControlTextarea1');
-        textarea.value = contenido;
-        console.log(textarea.value);
-      };
-  
-      lector.readAsText(archivo);
-    }
+function leerArchivo() {
+  var archivoInput = document.getElementById('archivoInput');
+  var archivo = archivoInput.files[0];
+
+  if (archivo) {
+    var lector = new FileReader();
+    lector.onload = function(e) {
+      var contenido = e.target.result;
+
+      // Guardar el contenido en una variable
+      var contenidoArchivo = contenido;
+
+      // Imprimir el contenido en la consola
+      console.log(contenidoArchivo);
+    };
+
+    lector.readAsText(archivo);
   }
+}
